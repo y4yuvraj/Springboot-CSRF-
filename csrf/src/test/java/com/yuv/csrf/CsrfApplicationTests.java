3c42fc1a932f9832f612c9278c76fde0d16778a6
yuvraj <46413950+y4yuvraj@users.noreply.github.com>
2024-05-10 19:21:49 +0530

    getting started with security

---
+++ csrf/src/test/java/com/yuv/csrf/CsrfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuv.csrf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsrfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
